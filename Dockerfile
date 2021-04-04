36fcfdfd8016a7003f7b758f60c9163dfcd1a75f
sonbarse <sonbarse@gmail.com>
2021-04-04 18:16:17 +0000

    all

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
 FROM java:8
-
   COPY . /var/www/java
-
   WORKDIR /var/www/java
-
   RUN javac Hello.java
-
   CMD ["java", "Hello"]
-
 
--- Hello.java
+++ Hello.java
@@ -1,11 +1,6 @@
 class Hello {
-
     public static void main(String[] args) {
-
       System.out.println("This is a java app created using Docker");
-
     }
-
   }
-
 
